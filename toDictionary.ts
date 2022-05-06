17534f92d250bfdb7726e53de0b34e13a4c962ad
Herman Persitz <herman@zeppelinlabs.io>
2022-05-06 09:42:53 -0300

    Improvement to typings

--- toDictionary.ts
+++ toDictionary.ts
@@ -1,12 +1,26 @@
-export type ObjDictionary<T> = {
-    [a: string]: T,
+export type ObjDictionary<K extends keyof any, T> = {
+    [a in K]: T
 }
-export const toDictionary = <T>(data: T[], getKey: (o: T) => string) => {
-    return data.reduce<ObjDictionary<T>>((acc, curr) => {
+
+export const toDictionary = <T, K extends keyof any>(data: T[], getKey: (o: T) => K) => {
+    return data.reduce((acc, curr) => {
         const key = getKey(curr)
         if (key) {
             acc[key] = curr
         }
         return acc
-    }, {})
+    }, {} as ObjDictionary<K, T>)
+}
+
+export const toDictionaryMap = <T, K extends keyof any, R>(
+    data: T[],
+    { key: getKey, value: getValue, }: { key: (o: T) => K, value: (o: T) => R }
+) => {
+    return data.reduce((acc, curr) => {
+        const key = getKey(curr)
+        if (key) {
+            acc[key] = getValue(curr)
+        }
+        return acc
+    }, {} as ObjDictionary<K, R>)
 }
